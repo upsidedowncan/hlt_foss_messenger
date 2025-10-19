93c1bf1e3be500a2083ff1502989fd833a3e153a
BankMannru <144546900+upsidedowncan@users.noreply.github.com>
2025-10-19 22:37:18 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "733897951"
+    serviceWorkerVersion: "4109191549"
   }
 });
